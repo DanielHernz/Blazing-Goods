308c1c4c1de7f0804025a6719114ead8db60939f
DanielHernz <dany.hernz.animation@gmail.com>
2021-08-06 02:11:27 -0500

    Probando getchild

--- Assets/Scripts/Secondary Props Scripts/Dispensador.cs
+++ Assets/Scripts/Secondary Props Scripts/Dispensador.cs
@@ -3,44 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dispensador : MonoBehaviour
-{
-    private bool TriggerON;
-    private GameObject PlayerGameObject;
-    private Transform PlayerTransform;
-    public Transform Hijo;
-
-    
+{    
     // Start is called before the first frame update
     void Start()
     {
-        PlayerGameObject = GameObject.FindGameObjectWithTag("Player");
-        PlayerTransform = PlayerGameObject.GetComponent<Transform>();
-    }
-    private void OnTriggerEnter(Collider collider) 
-    {
-        TriggerON = true;
-        Debug.Log("TriggerON ="+ TriggerON);
-    }
-    private void OnTriggerExit(Collider collider) 
-    {
-        TriggerON = false;
-        Debug.Log("TriggerON ="+ TriggerON);
+        
     }
     // Update is called once per frame
     void Update()
     {
-        if (TriggerON == true)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Hijo.SetParent(PlayerTransform);
-                Debug.Log("Levantar");  
-            } 
-            if (Input.GetMouseButtonDown(1))
-            {
-                Hijo.SetParent(gameObject.transform);
-                Debug.Log("Dejar");  
-            } 
-        }
+        
     }
 }
