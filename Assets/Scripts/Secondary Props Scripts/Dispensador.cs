c01cddd77c9e798b411f03edb3c0d193e306ac2a
DanielHernz <dany.hernz.animation@gmail.com>
2021-08-08 03:10:33 -0500

    Experimentando dispensador

--- Assets/Scripts/Secondary Props Scripts/Dispensador.cs
+++ Assets/Scripts/Secondary Props Scripts/Dispensador.cs
@@ -2,16 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dispensador : MonoBehaviour
-{    
+public class Dispensador : Mesas
+{   
+    public GameObject Prop_Dispensado;
+    Transform DispensadorPROP;
+    Vector3 Nueva_Posicion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Nueva_Posicion = transform.position;
     }
+
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (TriggerON == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(Prop_Dispensado,new Vector3(Nueva_Posicion), Prop_Dispensado.transform.rotation);
+                Debug.Log("1"); 
+                Prop_Dispensado.transform.SetParent(PlayerTransform);
+                Debug.Log("2"); 
+            } 
+        }
     }
 }
